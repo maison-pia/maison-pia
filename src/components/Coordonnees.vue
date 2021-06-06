54ca2819025adbf7e698457475a2eeeead0d2f2d
Estelle <estelle.thou@gmail.com>
2021-06-06 16:20:00 +0200

    Add references and banner

---
+++ src/components/Coordonnees.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mt-5">
+    <ul class="data">
+      <li style="font-weight: bold">Sokphea TAN</li>
+      <li class="mt-2">
+        <v-icon>{{ icons.mdiMapMarker }}</v-icon
+        >13 lotissement de la Lauzerdié 81110 VERDALLE
+      </li>
+      <li class="mt-2">
+        <v-icon>{{ icons.mdiCity }}</v-icon
+        >A 15 min de CASTRES
+      </li>
+      <li class="mt-2">
+        <v-icon>{{ icons.mdiPhone }}</v-icon
+        >07.70.99.63.25
+      </li>
+      <li class="mt-2">
+        <v-icon>{{ icons.mdiEmail }}</v-icon
+        >07.70.99.63.25
+      </li>
+      <li class="mt-2">A votre écoute !</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mdiMapMarker, mdiCity, mdiPhone, mdiEmail } from "@mdi/js";
+export default {
+  name: "Coordonnees",
+  components: {},
+  data() {
+    return {
+      icons: {
+        mdiMapMarker,
+        mdiCity,
+        mdiPhone,
+        mdiEmail,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital@1&display=swap");
+.data {
+  list-style-type: none;
+}
+
+@media (min-width: 1281px) {
+}
+@media (min-width: 1000px) and (max-width: 1280px) {
+}
+@media (min-width: 481px) and (max-width: 999px) {
+}
+@media (min-width: 200px) and (max-width: 480px) {
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,41 @@
 <template>
   <header>
     <div id="home" class="mt-5">
-      <v-parallax src="./../assets/IMG-20210606-WA0013.jpg">
+      <v-parallax src="./../assets/banner.webp">
         <v-row align="center" justify="center">
           <v-col class="text-center">
             <div>
               <p class="title-banner">
-                <span class="subtitle-banner"> COLOCATION SENIOR </span>
                 <span
                   class="col-8"
                   style="background-color: rgba(241, 148, 138, 0.9)"
                   >Bienvenue chez Maison Pia
                 </span>
+                <span class="subtitle-banner"> COLOCATION SENIOR </span>
               </p>
             </div>
           </v-col>
         </v-row>
       </v-parallax>
+      <div class="container">
+        <Coordonnees />
+      </div>
     </div>
   </header>
 </template>
 
 <script>
+import Coordonnees from "../components/Coordonnees.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { Coordonnees },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital@1&display=swap");
-
 .title-banner {
   font-family: "Bodoni Moda", serif !important;
-  margin-top: 10%;
-  font-size: 5rem;
   text-align: center;
   color: whitesmoke;
   display: flex;
@@ -45,8 +46,51 @@ export default {
 .subtitle-banner {
   font-family: "Lucida Sans Unicode", "Lucida Grande", "Lucida Sans", Arial,
     sans-serif;
-  color: whitesmoke;
-  font-size: 2.6rem;
+  color: white;
   font-weight: normal;
+  margin-top: 10%;
+}
+
+@media (min-width: 1281px) {
+  .title-banner {
+    margin-top: 10%;
+    font-size: 4rem;
+  }
+
+  .subtitle-banner {
+    font-size: 2.6rem;
+  }
+}
+
+@media (min-width: 1000px) and (max-width: 1280px) {
+  .title-banner {
+    margin-top: 10%;
+    font-size: 3.6rem;
+  }
+
+  .subtitle-banner {
+    font-size: 2.6rem;
+  }
+}
+
+@media (min-width: 481px) and (max-width: 999px) {
+  .title-banner {
+    margin-top: 10%;
+    font-size: 3.2rem;
+  }
+
+  .subtitle-banner {
+    font-size: 2.6rem;
+  }
+}
+@media (min-width: 200px) and (max-width: 480px) {
+  .title-banner {
+    margin-top: 10%;
+    font-size: 5rem;
+  }
+
+  .subtitle-banner {
+    font-size: 2.6rem;
+  }
 }
 </style>
